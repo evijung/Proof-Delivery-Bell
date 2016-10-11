70ef81717a2cb6028fd582b2295b051f9f2e4bcb
evijung <evides01@gmail.com>
2016-10-11 13:53:09 +0700

    Success1

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
@@ -5,34 +5,33 @@ import android.content.Context;
 import android.content.DialogInterface;
 
 /**
- * Created by tunyaporns on 10/11/2016.
+ * Created by musz on 10/11/2016.
  */
 
 public class MyAlert {
-    //Expicit
+    //Explicit
     private Context context;
 
     public MyAlert(Context context) {
         this.context = context;
     }
 
-    //Dialog for Error
+    // Dialog  for Error
     public void myErrorDialog(int intIcon,
-                              String strMessage,
-                              String strTitle) {
+                              String strTitle,
+                              String strMessage) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setCancelable(false);
         builder.setIcon(intIcon);
         builder.setTitle(strTitle);
         builder.setMessage(strMessage);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
-
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
             }
         });
         builder.show();
-
     }
-}
+
+}   // Main Class
--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyConstant.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyConstant.java
@@ -5,4 +5,21 @@ package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
  */
 
 public class MyConstant {
-}
+    //Explicit
+    private int iconAnInt = R.drawable.doremon48;// Image for icon 48x48
+    private String titleHaveSpaceString = "มีช่องว่าง";
+    private String messageHaveeSpaceString = "กรุณากรอกข้อมูลให้ครบทุกช่อง ค่ะ!";
+
+
+    public int getIconAnInt() {
+        return iconAnInt;
+    }
+
+    public String getTitleHaveSpaceString() {
+        return titleHaveSpaceString;
+    }
+
+    public String getMessageHaveeSpaceString() {
+        return messageHaveeSpaceString;
+    }
+}//Main Class
