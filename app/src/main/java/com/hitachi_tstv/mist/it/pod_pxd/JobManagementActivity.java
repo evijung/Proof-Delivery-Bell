daddbdc27403845eefdfcebadb9c072d63ef577b
evijung <evides01@gmail.com>
2017-06-05 10:31:51 +0700

    Before Doing new Menu 05/06/2017

--- app/src/main/java/com/hitachi_tstv/mist/it/pod_pxd/JobManagementActivity.java
+++ app/src/main/java/com/hitachi_tstv/mist/it/pod_pxd/JobManagementActivity.java
@@ -10,5 +10,7 @@ public class JobManagementActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_management);
+
+
     }
 }
